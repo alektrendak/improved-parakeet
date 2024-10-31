7794a414eca1fe349dd93a5fbca4e8bd1511f634
alektrendak <alek.trendak@gmail.com>
2024-10-31 09:54:05 +0100

    Test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for source_file in "test"/*.b; do
+    test_name=$(basename "$source_file" .b)
+    
+    python3 improved-parakeet.py "$source_file" > "test/${test_name}.tmp"
+    
+    if diff "test/${test_name}.tmp" "test/${test_name}.output" > /dev/null; then
+        echo "Test passed for $source_file"
+    else
+        echo "Test failed for $source_file"
+    fi
+    
+    rm "test/${test_name}.tmp"
+done
